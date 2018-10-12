a429a6892fe43bd9a1a88d25811dce41569cb19f
Jonah Spear <Jonahspear512@gmail.com>
2018-10-12 14:36:48 -0400

    Use thread to recieve messages

--- Chatroom/server.py
+++ Chatroom/server.py
@@ -1,4 +1,5 @@
 import socket
+from threading import Thread
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5000
@@ -6,6 +7,11 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
-while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+def recieve_messages():
+    # Receives messages on a port
+    while True:
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        print("received message:", data)
+
+t = Thread(target=recieve_messages)
+t.start()
