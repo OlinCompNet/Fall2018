231e69a4d23d46b7a7248bc2c8610a772dee043b
Jonah Spear <Jonahspear512@gmail.com>
2018-10-12 14:41:18 -0400

    Print address

--- Chatroom/server.py
+++ Chatroom/server.py
@@ -7,11 +7,14 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
+clients = set()
+
 def recieve_messages():
     # Receives messages on a port
     while True:
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-        print("received message:", data)
+        print("received message:", data.decode('UTF8'), "from", addr)
+
 
 t = Thread(target=recieve_messages)
 t.start()
