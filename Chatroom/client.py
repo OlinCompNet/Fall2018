a241034d9d368d262d8be0bd4e986cfc5c02d942
nathanieltan <nathaniel98.tan@gmail.com>
2018-10-12 14:44:14 -0400

    working client

--- Chatroom/client.py
+++ Chatroom/client.py
@@ -1,1 +1,25 @@
-from
+from socket import *
+from threading import Thread
+
+def recieve():
+    while True:
+        msg = client_socket.recv(1024).decode("utf8")
+        print(msg)
+
+def send():
+    while True:
+        msg = input()
+        client_socket.send(bytes(msg,"utf8"))
+
+
+ADDR = ('127.0.0.1', 5000)
+
+client_socket = socket(AF_INET, SOCK_DGRAM)
+client_socket.connect(ADDR)
+
+receive_thread = Thread(target=recieve)
+send_thread = Thread(target=send)
+receive_thread.start()
+send_thread.start()
+receive_thread.join()
+send_thread.join()
